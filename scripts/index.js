dd2951dd948e334494a609a6e31e710381598182
Demin Roman <romkademin@yandex.ru>
2021-05-30 16:12:39 +0300

    испрален html

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,28 @@
 let openPopup = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let closePopup = document.querySelector('.popup__close')
-let savePopup = document.querySelector('.popup__save')
+let closePopup = document.querySelector('.popup__close');
+let savePopup = document.querySelector('.form__save');
 
-let profileName = document.querySelector('.profile__name')
-let popupName = document.querySelector('.popup__name')
-let profileStatus = document.querySelector('.profile__role')
-let popupStatus = document.querySelector('.popup__role')
+let profileName = document.querySelector('.profile__name');
+let popupName = document.querySelector('popup__name');
+let profileStatus = document.querySelector('.profile__role');
+let popupStatus = document.querySelector('popup__role');
+
 
 function activePopup() {
     popupName.value = profileName.textContent;
     popupStatus.value = profileStatus.textContent;
-    popup.classList.add('.popup_opened')
+    popup.classList.add('popup_opened')
 }
 
 function closePopupForm() {
-    popup.classList.remove('.popup_opened')
+    popup.classList.remove('popup_opened')
 }
 
 function savePopupChanges(submit) {
     submit.preventDefault();
-    profileName.value = popupName.textContent;
-    profileStatus.value = popupStatus.textContent;
+    profileName.textContent = popupName.value;
+    profileStatus.textContent = popupStatus.value;
     closePopupForm();
 }
 
